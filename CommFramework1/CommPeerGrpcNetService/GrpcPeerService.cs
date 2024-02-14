58cdd11b81e9b1296ebe7d0791149bde37e91200
Stephen Pinto <stephen_pinto@outlook.com>
2024-02-14 09:49:14 +0530

    small changes

--- CommFramework1/CommPeerGrpcNetService/GrpcPeerService.cs
+++ CommFramework1/CommPeerGrpcNetService/GrpcPeerService.cs
@@ -6,11 +6,11 @@ namespace CommPeerGrpcNetService
 {
     public class GrpcPeerService : CommPeerService.CommPeerServiceBase
     {
-        public override Task<Message> MakeRequest(Message request, ServerCallContext context)
+        public override async Task<Message> MakeRequest(Message request, ServerCallContext context)
         {
             Console.WriteLine($"Received request from {request.From} to {request.To} with body = {request.Data}");
 
-            return Task.FromResult(new Message
+            return await Task.FromResult(new Message
             {
                 From = "Peer",
                 To = request.From,
@@ -18,11 +18,11 @@ namespace CommPeerGrpcNetService
             });
         }
 
-        public override Task<Message> Notify(Message request, ServerCallContext context)
+        public override async Task<Message> Notify(Message request, ServerCallContext context)
         {
             Console.WriteLine($"Received request from {request.From} to {request.To} with body = {request.Data}");
 
-            return Task.FromResult(new Message
+            return await Task.FromResult(new Message
             {
                 From = "Peer",
                 To = request.From,
